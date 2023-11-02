70307ae232b9a4f625c555e5556a864e41101bfe
Shatha Hroub <148472731+ShathaHroub@users.noreply.github.com>
2023-11-02 15:56:17 +0300

    Update ChatHub.cs

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,7 +4,7 @@ namespace Roamer.Hubs
 {
     public class ChatHub: Hub
     {
-        //sendmessage client method and receivemessage server side method content has to be the same
+        
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
